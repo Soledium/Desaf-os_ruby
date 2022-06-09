b36f5d81d15ec8cd4cc9614b5fdb2f8e750e6826
Soledium <nescobar3@hotmail.com>
2022-06-09 09:51:56 -0400

    organizando carpetas

---
+++ desafios miscelaneos/mayor_de_3.rb
@@ -0,0 +1,11 @@
+
+numero_uno = ARGV[0].to_i
+numero_dos = ARGV[1].to_i
+numero_tres = ARGV[2].to_i
+if numero_uno >= numero_dos && numero_uno >= numero_tres
+  puts numero_uno
+elsif numero_dos >= numero_uno && numero_dos >= numero_tres
+  puts numero_dos
+else
+  puts numero_tres
+end
