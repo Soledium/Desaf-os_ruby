7ec41491ab30ce7c60a7cb0baff99796b819ae61
Soledium <nescobar3@hotmail.com>
2022-06-14 11:20:02 -0400

    agregando nueva carpeta de desafios

---
+++ arreglos_archivos/proyecciones.rb
@@ -0,0 +1,19 @@
+
+data = open('ventas_base.db').read.chomp.split(',')
+print data
+puts ""
+ventas = []
+aumentoVentas = []
+
+#método?
+data.each do |i|
+    ventas.push i.to_i
+end
+print ventas
+ventas.each do |j|
+    aumentoVentas.push(j * 1.10)
+    print aumentoVentas
+end
+
+
+
