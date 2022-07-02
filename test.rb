39c800161f6d0748ad30cf0160e0436fd062ec57
Soledium <nescobar3@hotmail.com>
2022-07-01 21:34:29 -0400

    pruebas

--- test.rb
+++ test.rb
@@ -1,1 +1,12 @@
-hola de nuevo 
+#ejemplo de while
+# puts "ingresa un numero entre 1 y 10: "
+# num = gets.to_i
+# while num < 1|| num > 10
+#     puts "el numero no está entre 1 y 10"
+#     puts "ingresa un numero entre 1 y 10"
+#     num = gets.to_i
+# end
+
+# puts "El número ingresado fue #{num}."
+
+
